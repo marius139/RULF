67a5eb8b232047b6aed8aebbbbd34a31b9c2d3f5
AntonBock <34304728+AntonBock@users.noreply.github.com>
2018-12-17 15:36:00 +0100

    Add files via upload

---
+++ SettingUpTheThingBackup/gripperCommands.h
@@ -0,0 +1,30 @@
+
+int sel=1;
+int tdelay=10;
+int counter=0;
+int timer=0;
+unsigned long timestamp = 0;
+
+//Create a function which takes the input for signal of 1 and 2 from myo.
+void gripperCommands(){
+
+timer = millis();
+if (timer>(timestamp+5000)){
+  if(sel==0){
+        sendWriteInstruction4(4, 116, 3072);
+        delay(tdelay);
+        sendWriteInstruction4(5, 116, 1024);
+        delay(20);
+        sel=1; 
+        timestamp = millis();
+        }
+       else if(sel==1){
+        sendWriteInstruction4(4, 116, 2125);
+        delay(tdelay);
+        sendWriteInstruction4(5, 116, 1975);
+        delay(20);
+        sel=0;
+        timestamp = millis();
+        }
+}
+}
